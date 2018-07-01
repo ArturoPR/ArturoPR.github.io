b09dadb30d0bb7a901037a00ff7dc6a1d1e1b675
ArturoPR <artu.proton@gmail.com>
2018-07-02 00:38:01 +0200

    lol

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 var javi;
 
+var muerto;
+
 var lol;
 var song;
+
+var sound;
 
 var lol1, lol2, lol3, peke1, peke2, peke3;
 var xball, yball, bvel,x,y;
@@ -20,7 +24,7 @@ var img3;
 var img4;
 var img5;
 var img6;
-var javiPlay;
+var javiPlay,full;
 
 var fx,fy;
 var showplay1,showplay2,showplay3;
@@ -34,9 +38,13 @@ var vely;
     
 var img5;
 
+var s0,s1,s2,s3,s4;
+
+var playx,playy;
+
 function preload(){
   lol = loadSound('data/pumoof.mp3');
-  song = loadSound('If_I_Had_a_Chicken.mp3');
+  song = loadSound('data/If_I_Had_a_Chicken.mp3');
   
   wow = loadImage("data/sebas1.png");
   img2 = loadImage("data/sebas2.png");
@@ -45,12 +53,23 @@ function preload(){
   img5 = loadImage("data/javi.png");
   img6 = loadImage("data/sebas mini.png");
   javiPlay = loadImage("data/play javi.png");
+
+  s0 = loadImage("data/sound0.png");
+  s1 = loadImage("data/sound1.png");
+  s2 = loadImage("data/sound2.png");
+  s3 = loadImage("data/sound3.png");
+  s4 = loadImage("data/sound4.png");
   
   img5 = loadImage("data/javi.png");
+  full = loadImage("data/full.png");
 }
 
 function setup(){
-  translate(width/2,height/2);
+  muerto=false;
+  sound=2;
+  song.setVolume(1);
+  lol.setVolume(1);
+  //translate(width/2,height/2);
   sioq=0;
   yesorwat=0;
   punt = 4;
@@ -60,13 +79,8 @@ function setup(){
   vely=random(-20,20);
   
   song.loop();
-  createCanvas(displayWidth,displayHeight);
-  for (var i = 0; i < javis.length; i++) {
-    javis[i] = new javi();
-  }
-  
-
-  show=0;
+  createCanvas(displayWidth-20,displayHeight-20);
+show=0;
   show2=255;
   dific1=5+punt/5;
   dific2=7+punt/5;
@@ -76,9 +90,9 @@ function setup(){
   yballmalo=random (height/15,height/15*14);
   xballmalo=width;
   yballmalo=random (height/15,height/15*14);
-  col1 = random (0,200);
-  col2 = random (0,200);
-  col3 = random (0,200);
+  col1 = 0;
+  col2 = 0;
+  col3 = 0;
   bvel = random(dific1,dific2);
   
   pausa1=false;
@@ -86,14 +100,27 @@ function setup(){
   }
 
 function draw (){ 
+  if(sioq>0){
+    col1=random(0,255);
+    col2=random(0,255);
+    col3=random(0,255);
+  }else{
+    col1 = 0;
+    col2 = 0;
+    col3 = 0;
+  }
+  if(mouseIsPressed){
+    noCursor();
+  }else{
+    cursor(ARROW);
+  }
   dific1=5+punt/5;
   dific2=7+punt/5;
   translate(0,0);
   background(col1,col2,col3);
   textSize(100);
-  fill(showplay1,showplay2,showplay3);
+  fill(255,255,255);
   textAlign(CENTER, CENTER);
-  text("PLAY",width/2,height/10*8);
   
   textSize(100);
   textAlign(CENTER, CENTER);
@@ -109,6 +136,12 @@ function draw (){
     fill(255);
     textAlign(CENTER, CENTER);
     text("Game Over", width/2,height/2);
+    textSize(100);
+    fill(255,255,255);
+    textAlign(CENTER, CENTER);
+    if(sioq<1){
+    text("PLAY",width/2,height/10*8);
+  }
     xball=width*2;
     xrep=width*2;
     xballmalo=-width*2;
@@ -119,19 +152,8 @@ function draw (){
       javiPlay.resize(255,99);
       image(javiPlay,width/2,height/10*8);
     }
-    if(touchY>height/10*7 && mouseIsPressed && touchX > width/2-width/10*3 && touchX < width/2+width/10*3){
-      xball=0;
-      yball=random (height/15,height/15*14);
-      xballmalo=0;
-      yballmalo=random (height/15,height/15*14);
-      xrep=width;
-      yrep=random (height/15,height/15*14);
-      dific1=5+punt/5;
-      dific2=7+punt/5;
-      bvel = random(dific1,dific2);
-      show=0;
-      show2=255;
-      punt=0;
+    if(mouseY>height/10*7 && mouseIsPressed && mouseX > width/2-width/10*3 && mouseX < width/2+width/10*3){
+      muerto=true;
     }
  
     }else{
@@ -184,31 +206,22 @@ function draw (){
     fill(255);
     textAlign(CENTER, CENTER);
     text("Game Over", width/2,height/2);
+    textSize(100);
+    fill(255,255,255);
+    textAlign(CENTER, CENTER);
+    if(sioq<1){
+    text("PLAY",width/2,height/10*8);
+  }
     xball=width*2;
     xrep=width*2;
     xballmalo=-width*2;
     show=255;
     show2=0;
-    if(touchY>height/10*7 && mouseIsPressed && touchX > width/2-width/10*3 && touchX < width/2+width/10*3){
-      xball=0;
-      yball=random (height/15,height/15*14);
-      xballmalo=0;
-      yballmalo=random (height/15,height/15*14);
-      xrep=width;
-      yrep=random (height/15,height/15*14);
-      dific1=5+punt/5;
-      dific2=7+punt/5;
-      bvel = random(dific1,dific2);
-      show=0;
-      show2=255;
-      punt=0;
+    if(mouseY>height/10*7 && mouseIsPressed && mouseX > width/2-width/10*3 && mouseX < width/2+width/10*3){
+      muerto=true;
     }
   } else{
     xballmalo+=bvel;
-  }
-  
-  for (var i = 0; i < javis.length; i++) {
-  javi.how();
   }
   
   imageMode(CENTER);
@@ -221,8 +234,8 @@ function draw (){
   image(img3,xrep,yrep);
   
   if(mouseIsPressed){
-    fx=touchX;
-    fy=touchY;
+    fx=mouseX;
+    fy=mouseY;
    imageMode(CENTER);
    img4.resize(200,200);
    image(img4,fx,fy);
@@ -233,13 +246,73 @@ function draw (){
    }
   
   //fill(255);
-  //ellipse(touchX,touchY,150,150);
-  
+  //ellipse(mouseX,mouseY,150,150);
+  stroke(255);
+  strokeWeight(2);
+  fill(0);
+  rect(width-400,0,50,50);
+  rect(width-475,0,50,50);
+  rect(width-550,0,50,50);
+  rect(width-625,0,50,50);
+  rect(width-700,0,50,50);
+
+  if(mouseX>=width-700 && mouseX<=width-650 && mouseY<=50 && mouseIsPressed){
+    sound=0;
+    song.setVolume(0);
+    lol.setVolume(0);
+  }
+  if(mouseX>=width-625 && mouseX<=width-575 && mouseY<=50 && mouseIsPressed){
+    sound=1;
+    song.setVolume(0.1);
+    lol.setVolume(0.1);
+  }
+  if(mouseX>=width-550 && mouseX<=width-500 && mouseY<=50 && mouseIsPressed){
+    sound=2;
+    song.setVolume(1);
+    lol.setVolume(1);
+  }
+  if(mouseX>=width-475 && mouseX<=width-425 && mouseY<=50 && mouseIsPressed){
+    sound=3;
+    song.setVolume(5);
+    lol.setVolume(5);
+  }
+  if(mouseX>=width-400 && mouseX<=width-350 && mouseY<=50 && mouseIsPressed){
+    sound=4;
+    song.setVolume(324376);
+    lol.setVolume(3132456);
+  }
+  if(sound==0){
+    imageMode(CORNER);
+    s0.resize(35,45);
+    image(s0,width-692,2.5);
+  }if(sound==1){
+    imageMode(CORNER);
+    s1.resize(35,45);
+    image(s1,width-617,2.5);
+  }if(sound==2){
+    imageMode(CORNER);
+    s2.resize(35,45);
+    image(s2,width-542,2.5);
+  }if(sound==3){
+    imageMode(CORNER);
+    s3.resize(35,45);
+    image(s3,width-467,2.5);
+  }if(sound==4){
+    imageMode(CORNER);
+    s4.resize(35,45);
+    image(s4,width-392,2.5);
+  }
+
+  fill(255);
+  textSize(30);
+  text("Sound Level",width-800,25);
+  text("Full Screen",160,25);
+
   stroke(255);
   strokeWeight(2);
   fill(0);
   rect(width-110,0,50,50);
-  if(touchX>=width-110 && touchY<=50 && mouseIsPressed){
+  if(mouseX>=width-110 && mouseY<=50 && mouseIsPressed){
     sioq=0;
   }
   
@@ -247,7 +320,7 @@ function draw (){
   strokeWeight(2);
   fill(0);
   rect(width-50,0,50,50);
-  if(touchX>=width-50 && touchY<=50 && mouseIsPressed){
+  if(mouseX>=width-50 && mouseY<=50 && mouseIsPressed){
     sioq=1;
   }
   
@@ -260,31 +333,31 @@ function draw (){
     img5.resize(35,45);
     image(img5,width-42,2.5);
   } 
-  if(touchX>width/10-30){
+  if(mouseX>width/10-30){
     pos1=true;
   }else{
     pos1=false;
   }
   
-  if(touchX<width/10+50+30){
+  if(mouseX<width/10+50+30){
     pos2=true;
   }else{
     pos2=false;
   }
   
-  if(touchY>width/10-30){
+  if(mouseY>width/10-30){
     pos3=true;
   }else{
     pos3=false;
   }
   
-  if(touchY<width/10+50+30){
+  if(mouseY<width/10+50+30){
     pos4=true;
   }else{
     pos4=false;
   }
   
-  if(touchX>width/10-30 && touchX<width/10+50+30 && touchY>width/10-30 && touchY<width/10+50+30 && pausa2==false){
+  if(mouseX>width/10-30 && mouseX<width/10+50+30 && mouseY>width/10-30 && mouseY<width/10+50+30 && pausa2==false){
     pausa1=true;
   }else{
     pausa1=false;
@@ -306,6 +379,7 @@ function draw (){
     triangle(width/10,(width/10)-5,width/10,(width/10)+55,(width/10)+50,(width/10)+25);
     bvel=0;
   }
+  image(full,10,10);
 }
 
 function javi(){
@@ -331,6 +405,32 @@ function javi(){
       showplay1=show;
       showplay2=show;
       showplay3=show;
+      
     }
+  }
+}
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 70 && mouseY > 0 && mouseY < 70) {
+    var fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
+
+function mouseReleased(){
+  if(muerto==true){
+    xball=0;
+    yball=random (height/15,height/15*14);
+    xballmalo=0;
+    yballmalo=random (height/15,height/15*14);
+    xrep=width;
+    yrep=random (height/15,height/15*14);
+    dific1=5+punt/5;
+    dific2=7+punt/5;
+    bvel = random(dific1,dific2);
+    show=0;
+    show2=255;
+    punt=0;
+    muerto=false;
   }
 }
