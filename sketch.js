e051deb50ae4f0339414790950825ca3d92e90ac
ArturoPR <artu.proton@gmail.com>
2018-07-01 13:32:15 +0200

    a

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-var img;  // Declare variable 'img'.
+var song;
 
 function setup() {
-  createCanvas(720, 400);
-  img = loadImage("data/javi.png");  // Load the image
+  song = loadSound('data/If_I_Had_a_Chicken.mp3');
+  createCanvas(720, 200);
+  background(255,0,0);
 }
 
-function draw() {
-  // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
-  // Displays the image at point (0, height/2) at half size
-  image(img, 0, height/2, img.width/2, img.height/2);
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
 }
